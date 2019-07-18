0fac45ebcf5122182696851a6bdc54a8a25fdb0b
Alexander Stefanov <alexander@mezon.ru>
2019-07-19 02:11:34 +0300

    add more code

--- finder.py
+++ finder.py
@@ -1,37 +1,30 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import requests
 import re
-import json
-import argparse
+import random
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+import time
+from selenium import webdriver
 from bs4 import BeautifulSoup
 
 
 def ll_request(book):
-    try:
-        search_headers = {'User-Agent': 'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.2171.95 Safari/537.36', 'Origin': 'https://www.livelib.ru',
-                          'Upgrade-Insecure-Requests': '1', 'DNT': '1'}
-        payload = {'btn_search': 'Искать', 'search[text]': '{}'.format(book)}
-        print(payload)
-        URL = 'https://www.livelib.ru/find'
-        r = requests.post(url=URL,data=json.dumps(payload), headers=search_headers)
-        page = r.content.decode('utf-8')
-        print(page)
-        soup = BeautifulSoup(page, 'html.parser')
-        book_list = soup.find(
-            'div', {'class': 'object-wrapper object-wrapper-outer object-edition'})
-        template = '{prefix}{url}'.format
-        PREFIX = 'https://www.livelib.ru'
-        links = [template(prefix=PREFIX, url=e.get('href'), title=e.get('title'))
-                 for e in book_list.find_all('a', href=True)]
-        print(links[0])
-    except: pass
+    driver = webdriver.Chrome('/usr/bin/chromedriver')
+    el = WebDriverWait(driver, 15)
+
+    payload = {'btn_search': 'Искать', 'search[text]': '{}'.format(book)}
+    URL = 'https://www.livelib.ru/find'
+    r = requests.post(url=URL,data=json.dumps(payload), headers=search_headers)
+    page = r.content.decode('utf-8')
+    soup = BeautifulSoup(page, 'html.parser')
+    book_list = soup.find(
+        'div', {'class': 'object-wrapper object-wrapper-outer object-edition'})
+    template = '{prefix}{url}'.format
+    PREFIX = 'https://www.livelib.ru'
+    links = [template(prefix=PREFIX, url=e.get('href'), title=e.get('title'))
+             for e in book_list.find_all('a', href=True)]
+    print(links[0])
 
 
 ll_request('Стеклянные пчелы')
-#ll_request('Рыцарь нищеты')
-#ll_request('В стальных грозах')
-#ll_request('Делалой М. Усы и юбки')
-#ll_request('Витте С.Ю. Воспоминания, очерки, дневники, письма')
-#ll_request('Жан-Пьер Руссо "Нездешняя гавань"')
-#ll_request('Нестор Махно. Воспоминания')
--- scraper.py
+++ scraper.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 import requests
 import re
@@ -8,8 +8,9 @@ import argparse
 from bs4 import BeautifulSoup
 
 
+headers = {
+    'User-Agent': 'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.2171.95 Safari/537.36'}
 
-headers = {'User-Agent': 'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.2171.95 Safari/537.36'}
 
 def parse_book_id(url):
     print(url)
@@ -33,13 +34,15 @@ def parse_book_id(url):
 #        book_rating = soup.find('span')
         book_rating = soup.select_one('span.rating-book > span >  span')
 #        print(book_rating['title'])
-        print('book id: [{}] rating: [{}]'.format(book_id, book_rating['title']))
+        print('book id: [{}] rating: [{}]'.format(
+            book_id, book_rating['title']))
         return api_key.group(1), book_id, book_rating['title']
 
 
 def parse_concrete_book(url):
     api_key, book_id, book_rating = parse_book_id(url)
-    url = 'https://www.pricelib.ru/api/bookprice?callback=plcallback&api_key={}&edition_id={}'.format(api_key, book_id)
+    url = 'https://www.pricelib.ru/api/bookprice?callback=plcallback&api_key={}&edition_id={}'.format(
+        api_key, book_id)
     resp = requests.get(url, headers=headers)
     if resp.status_code == 404:
         print('requested page [{}] not found'.format(url))
@@ -52,23 +55,31 @@ def parse_concrete_book(url):
 #        print(cleanup)
         book_prices = json.loads(cleanup)
         # book is available and it is paper
-        a = list(filter(lambda x: x['is_available'] == "1" and x['type'] == 'paper', book_prices['data']))
+        a = list(filter(lambda x: x['is_available'] ==
+                        "1" and x['type'] == 'paper', book_prices['data']))
 #        print(json.dumps(a))
         for json_dict in a:
-            print('{} {}'.format(json_dict['shop_title'], json_dict['price_discount']))
+            print('{} {}'.format(
+                json_dict['shop_title'], json_dict['price_discount']))
 #            print("%s, цена %s " % (json_dict['shop_title'], json_dict['price_discount']))
 
 #        print(book_prices)
 #        print(json.dumps(book_prices, indent=4, sort_keys=True))
-#def livelib_finder(book):
+# def livelib_finder(book):
+
 
 def ll_request(book):
     try:
-        search_headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36', 'Origin': 'https://www.livelib.ru',
-                          'Upgrade-Insecure-Requests': '1', 'DNT': '1'}
+        search_headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36',
+                          'Origin': 'https://www.livelib.ru',
+                          'Upgrade-Insecure-Requests': '1', 'DNT': '1',
+                          'Content-Type': 'application/x-www-form-urlencoded',
+                          'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3'}
+
         payload = {'btn_search': 'Искать', 'search[text]': '{}'.format(book)}
         URL = 'https://www.livelib.ru/find'
-        r = requests.post(url=URL, data=json.dumps(payload), headers=search_headers)
+        r = requests.post(url=URL, data=json.dumps(
+            payload), headers=search_headers)
         page = r.content.decode('utf-8')
         print(page)
         soup = BeautifulSoup(page, 'html.parser')
@@ -80,10 +91,11 @@ def ll_request(book):
                  for e in book_list.find_all('a', href=True)]
         print(links[0])
         return links[0]
-    except: pass
+    except:
+        pass
 
 
-#parse_book_id("https://www.livelib.ru/book/1002813399-geliopol-ernst-yunger")
+# parse_book_id("https://www.livelib.ru/book/1002813399-geliopol-ernst-yunger")
 #parse_concrete_book('e63c683a3cd6a4c41e82dc4b718e0ff8', '1002813399')
 
 if __name__ == '__main__':
@@ -92,10 +104,10 @@ if __name__ == '__main__':
     parser.add_argument('--file', help='book list')
     args = parser.parse_args()
     if args.book is not None:
-#        parse_book_id(args.book)
+        #        parse_book_id(args.book)
         parse_concrete_book(args.book)
     if args.file is not None:
         with open(args.file) as file:
             for book in file:
-                print(book)
+                #print(book)
                 ll_request(book)
